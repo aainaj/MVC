a104105816e09c1af1476809efb610543478f943
Aaina Jain <aainajain100@gmail.com>
2018-06-26 04:42:22 +0530

    Project Initial Setup

---
+++ MVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVC
+//
+//  Created by Jain, Aaina on 6/26/18.
+//  Copyright © 2018 Jain, Aaina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
